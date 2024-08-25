f4b77547e60d824971a8d962869d5678d2f1e8eb
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-08-25 10:44:57 -0700

    feat: support dev.sh to envoke new shells

--- dev.sh
+++ dev.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker buildx build \
-    --build-arg "GROUP_ID=$(id -g)" \
-    --build-arg "USER_ID=$(id -u)" \
-    --tag "ghcr.io/alt-f4-llc/vorpal:edge-dev" \
-    --target "dev" \
-    .
+if [ "${VORPAL_DEV:-}" != "$PWD" ]; then
+    export VORPAL_DEV="$PWD"
 
-source "${PWD}/.env"
+    docker buildx build \
+        --build-arg "GROUP_ID=$(id -g)" \
+        --build-arg "USER_ID=$(id -u)" \
+        --tag "ghcr.io/alt-f4-llc/vorpal:edge-dev" \
+        --target "dev" \
+        .
+
+    export PATH=$PWD/.vorpal/bin:$PATH
+
+    if [ $# -gt 0 ]; then
+        exec "$@"
+    else
+        exec "$SHELL"
+    fi
+fi
