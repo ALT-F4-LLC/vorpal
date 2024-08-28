b2a58770a7a6d5a3d5b67c14adbd7630b1b64481
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-08-27 23:03:57 -0700

    chore(dev.sh): remove local cache options and --load flag from docker buildx command
    This update simplifies the docker buildx command by removing the local cache options and the --load flag, relying solely on the registry cache.

--- dev.sh
+++ dev.sh
@@ -2,11 +2,8 @@
 set -euo pipefail
 
 docker buildx build \
-    --cache-from "type=local,src=${PWD}/.vorpal/buildx" \
     --cache-from "type=registry,ref=ghcr.io/alt-f4-llc/vorpal-dev:edge-cache" \
-    --cache-to "type=local,dest=${PWD}/.vorpal/buildx,compression=zstd,compression-level=22,image-manifest=true,oci-mediatypes=true,mode=max" \
     --file "Dockerfile.dev" \
-    --load \
     --tag "ghcr.io/alt-f4-llc/vorpal-dev:edge" \
     .
 
