fe1e62706a3030701f43423695f972203546a55b
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-08-27 23:22:07 -0700

    refactor(workflow): update docker build steps and cache handling
    - Added BUILDX_CACHE_FROM environment variable to code-quality and package jobs.
    - Removed redundant docker build-push-action steps from code-quality and package jobs.
    - Updated dev.sh to use BUILDX_CACHE_FROM environment variable for cache-from.
    - Added cache-from and load options to docker buildx build command in dev.sh.

--- dev.sh
+++ dev.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ -z "${BUILDX_CACHE_FROM:-}" ]; then
+    BUILDX_CACHE_FROM="type=registry,ref=ghcr.io/alt-f4-llc/vorpal-dev:edge-cache"
+fi
+
+echo "Cache from: ${BUILDX_CACHE_FROM}"
+
 docker buildx build \
-    --cache-from "type=registry,ref=ghcr.io/alt-f4-llc/vorpal-dev:edge-cache" \
+    --cache-from "${BUILDX_CACHE_FROM}" \
     --file "Dockerfile.dev" \
+    --load \
     --tag "ghcr.io/alt-f4-llc/vorpal-dev:edge" \
     .
 
