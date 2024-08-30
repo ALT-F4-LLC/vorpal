03b83ab452ec8fcb203d40dcfd4171823e43973c
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-08-30 00:31:58 -0700

    refactor: update paths and dependencies for build and dev environments
    - Updated .envrc to modify PATH and OPENSSL_DIR variables.
    - Removed redundant environment variables from GitHub Actions workflow.
    - Added just installation function in dev.sh.
    - Updated openssl and protoc installation functions in dev.sh.
    - Adjusted paths and directory checks in dev.sh.

--- dev.sh
+++ dev.sh
@@ -1,22 +1,56 @@
 #!/usr/bin/env bash
 set -e
 
-OPENSSL_URL="https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERSION}/openssl-${OPENSSL_VERSION}.tar.gz"
-OPENSSL_VERSION="3.3.1"
-PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-osx-aarch_64.zip"
-PROTOC_VERSION="28.0"
+ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 WORKDIR=$(pwd)
 
+export NICKEL_IMPORT_PATH="$WORKDIR/.vorpal/packages:$WORKDIR"
+export OPENSSL_DIR="$WORKDIR/deps/openssl"
+export PATH="$WORKDIR/deps/proto/bin:$WORKDIR/deps/just:$HOME/.cargo/bin:$PATH"
+
+just() {
+    JUST_SYSTEM=""
+    JUST_VERSION="1.35.0"
+
+    if [[ "${OS}" == "darwin" ]]; then
+        JUST_SYSTEM="apple-darwin"
+    elif [[ "${OS}" == "linux" ]]; then
+        JUST_SYSTEM="unknown-linux-gnu"
+    else
+        echo "Unsupported OS: ${OS}"
+        exit 1
+    fi
+
+    if [[ "${ARCH}" == "x86_64" ]]; then
+        JUST_SYSTEM="x86_64-${JUST_SYSTEM}"
+    elif [[ "${ARCH}" == "arm64" ]]; then
+        JUST_SYSTEM="aarch64-${JUST_SYSTEM}"
+    else
+        echo "Unsupported ARCH: ${ARCH}"
+        exit 1
+    fi
+
+    JUST_URL="https://github.com/casey/just/releases/download/${JUST_VERSION}/just-${JUST_VERSION}-${JUST_SYSTEM}.tar.gz"
+
+    mkdir -p deps/just
+
+    curl -L "${JUST_URL}" -o deps/just-${JUST_VERSION}-${JUST_SYSTEM}.tar.gz
+
+    tar -xvf deps/just-${JUST_VERSION}-${JUST_SYSTEM}.tar.gz -C deps/just
+}
+
 openssl() {
-    wget "${OPENSSL_URL}" -O deps/openssl-${OPENSSL_VERSION}.tar.gz
+    OPENSSL_VERSION="3.3.1"
+    OPENSSL_URL="https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERSION}/openssl-${OPENSSL_VERSION}.tar.gz"
+
+    curl -L "${OPENSSL_URL}" -o deps/openssl-${OPENSSL_VERSION}.tar.gz
 
     tar -xvf deps/openssl-${OPENSSL_VERSION}.tar.gz -C deps
 
     cd deps/openssl-${OPENSSL_VERSION}
 
-    mkdir -p "${WORKDIR}/deps/openssl-${OPENSSL_VERSION}-dist"
-
-    ./Config --prefix="${WORKDIR}/deps/openssl-${OPENSSL_VERSION}-dist"
+    ./Config --prefix="${WORKDIR}/deps/openssl"
 
     make -j"$(nproc)"
 
@@ -24,9 +58,8 @@ openssl() {
 }
 
 protoc() {
-    ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
-    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
     PROTOC_SYSTEM=""
+    PROTOC_VERSION="28.0"
 
     if [[ "${OS}" == "darwin" ]]; then
         PROTOC_SYSTEM="osx"
@@ -46,22 +79,33 @@ protoc() {
         exit 1
     fi
 
-    wget "${PROTOC_URL}" -O deps/protoc-${PROTOC_VERSION}-${PROTOC_SYSTEM}.zip
+    if [[ "$PROTOC_SYSTEM" == "" ]]; then
+        echo "PROTOC_SYSTEM is empty"
+        exit 1
+    fi
 
-    mkdir -p deps/proto-${PROTOC_VERSION}-${PROTOC_SYSTEM}
+    PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-${PROTOC_SYSTEM}.zip"
 
-    unzip \
-        deps/protoc-${PROTOC_VERSION}-${PROTOC_SYSTEM}.zip \
-        -d deps/proto-${PROTOC_VERSION}-${PROTOC_SYSTEM}
+    curl -L "${PROTOC_URL}" -o deps/protoc-${PROTOC_VERSION}-${PROTOC_SYSTEM}.zip
+
+    rm -rf deps/proto
+
+    mkdir -p deps/proto
+
+    unzip deps/protoc-${PROTOC_VERSION}-${PROTOC_SYSTEM}.zip -d deps/proto
 }
 
 mkdir -p ./deps
 
-if [[ ! -d "./deps/openssl-${OPENSSL_VERSION}" ]]; then
+if [[ ! -d "./deps/just" ]]; then
+    just
+fi
+
+if [[ ! -d "./deps/openssl" ]]; then
     openssl
 fi
 
-if [[ ! -d "./deps/proto-${PROTOC_VERSION}-osx_aarch_64" ]]; then
+if [[ ! -d "./deps/proto" ]]; then
     protoc
 fi
 
@@ -76,3 +120,5 @@ if ! command -v rustc &> /dev/null || [[ ! -x "$(command -v rustc)" ]]; then
 fi
 
 cargo install nickel-lang-cli
+
+"$@"
