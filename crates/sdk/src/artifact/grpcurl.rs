aab33ead6394df4d0dc9b954bc49975cee5d7eef
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-04-11 12:53:40 -0700

    fix(lint)

--- crates/sdk/src/artifact/grpcurl.rs
+++ crates/sdk/src/artifact/grpcurl.rs
@@ -15,8 +15,9 @@ pub async fn build(context: &mut ConfigContext) -> Result<String> {
     let name = "grpcurl";
 
     let source_version = "1.9.3";
-    let source_path =
-        format!("https://github.com/fullstorydev/grpcurl/archive/refs/tags/v{source_version}.tar.gz");
+    let source_path = format!(
+        "https://github.com/fullstorydev/grpcurl/archive/refs/tags/v{source_version}.tar.gz"
+    );
     let source_digest = "3db5cef04f38e71c4007ed96cc827209ae5a1b6613c710cd656a252fafcde86c";
 
     let source = ArtifactSourceBuilder::new(name, &source_path)
