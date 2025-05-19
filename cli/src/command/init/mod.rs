b009fe61fa53322c789cf99e717ab64033fc7e94
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-05-19 12:59:48 -0700

    feat(sdk): updates all sdks with latest versions

--- cli/src/command/init/mod.rs
+++ cli/src/command/init/mod.rs
@@ -34,20 +34,12 @@ pub async fn run(level: Level) -> Result<()> {
     match answer {
         Ok(choice) => match choice {
             "Go" => {
-                template.insert(
-                    "cmd/example/main.go",
-                    include_str!("template/go/cmd/example/main.go"),
-                );
-
-                template.insert(
-                    "cmd/vorpal/main.go",
-                    include_str!("template/go/cmd/vorpal/main.go"),
-                );
-
+                template.insert("Vorpal.lock", include_str!("template/go/Vorpal.lock"));
                 template.insert("Vorpal.toml", include_str!("template/go/Vorpal.toml"));
-
                 template.insert("go.mod", include_str!("template/go/go.mod"));
                 template.insert("go.sum", include_str!("template/go/go.sum"));
+                template.insert("main.go", include_str!("template/go/main.go"));
+                template.insert("vorpal.go", include_str!("template/go/vorpal.go"));
             }
 
             "Rust" => {
--- cli/src/command/init/template/rust/vorpal/src/main.rs
+++ cli/src/command/init/template/rust/vorpal/src/main.rs
@@ -1,12 +1,22 @@
 use anyhow::Result;
-use vorpal_sdk::{artifact::language::rust::RustBuilder, context::get_context};
+use vorpal_sdk::{
+    api::artifact::{
+        ArtifactSystem,
+        ArtifactSystem::{Aarch64Darwin, Aarch64Linux, X8664Darwin, X8664Linux},
+    },
+    artifact::language::rust::RustBuilder,
+    context::get_context,
+};
+
+const SYSTEMS: [ArtifactSystem; 4] = [Aarch64Darwin, Aarch64Linux, X8664Darwin, X8664Linux];
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let context = &mut get_context().await?;
 
-    RustBuilder::new("example")
+    RustBuilder::new("example", SYSTEMS.to_vec())
         .with_bins(vec!["example"])
+        .with_includes(vec!["example", "Cargo.lock", "Cargo.toml"])
         .with_packages(vec!["example"])
         .build(context)
         .await?;
