b3a29c821cbd52776529eb8165a3795b7f0ef86d
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-05-30 19:39:47 +0100

    docs: finish writing binary install guide

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					autogenerate: { directory: 'reference' },
+					autogenerate: { directory: 'guides' },
 				},
 				{
 					label: 'Reference',
