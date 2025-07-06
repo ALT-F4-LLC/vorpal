2a240aedbed5ee9e228dd303ff2ed983700b7892
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-07-06 00:14:26 -0700

    feat(github): uses new setup-vorpal-action

--- .github/actions/setup-vorpal/__fixtures__/exec.ts
+++
@@ -1,4 +0,0 @@
-import * as exec from "@actions/exec";
-import { jest } from "@jest/globals";
-
-export const exec_fn = jest.fn<typeof exec.exec>();
--- .github/actions/setup-vorpal/__fixtures__/fs.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from "fs";
-import { jest } from "@jest/globals";
-
-export const openSync = jest.fn<typeof fs.openSync>();
-export const closeSync = jest.fn<typeof fs.closeSync>();
-export const existsSync = jest.fn<typeof fs.existsSync>();
-export const readFileSync = jest.fn<typeof fs.readFileSync>();
