b50472335bd94e97b5cca3655d817ca5c91ec7f5
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-07-06 20:39:53 -0700

    fix(cargo): update hash for intel darwin

--- sdk/rust/src/artifact/cargo.rs
+++ sdk/rust/src/artifact/cargo.rs
@@ -12,7 +12,7 @@ pub async fn build(context: &mut ConfigContext) -> Result<String> {
     let source_digest = match system {
         Aarch64Darwin => "7288c982cdf90e7bff441cda961ad8bb71ca011c8c14671a01f78703f19156a7",
         Aarch64Linux => "0000000000000000000000000000000000000000000000000000000000000000",
-        X8664Darwin => "0000000000000000000000000000000000000000000000000000000000000000",
+        X8664Darwin => "046747b1e1f14c76dac70bb280f0888f07fdd1b85fabb95f6aecbfa362d58bb7",
         X8664Linux => "0000000000000000000000000000000000000000000000000000000000000000",
         _ => bail!("unsupported {name} system: {}", system.as_str_name()),
     };
