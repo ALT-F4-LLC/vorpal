8c8d7d54eb3e966351fe2aedaa1811c8fb3e2400
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2026-01-20 16:56:20 -0500

    refactor(sdk/go): modularize artifact builders into separate files
    Split monolithic main.go into dedicated artifact files with lazy
    evaluation via switch statement, improving build performance by only
    building the requested artifact.

---
+++ sdk/go/cmd/vorpal/artifact/vorpal_process.go
@@ -0,0 +1,31 @@
+package artifact
+
+import (
+	"fmt"
+
+	"github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/artifact"
+	"github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/config"
+)
+
+func BuildVorpalProcess(context *config.ConfigContext) (*string, error) {
+	vorpal, err := BuildVorpal(context)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build vorpal: %w", err)
+	}
+
+	return artifact.NewProcess(
+		"vorpal-process",
+		fmt.Sprintf("%s/bin/vorpal", artifact.GetEnvKey(vorpal)),
+		SYSTEMS,
+	).
+		WithArguments([]string{
+			"--registry",
+			"https://localhost:50051",
+			"services",
+			"start",
+			"--port",
+			"50051",
+		}).
+		WithArtifacts([]*string{vorpal}).
+		Build(context)
+}
